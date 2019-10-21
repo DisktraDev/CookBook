dea95f75dc5b75964f0bf77f24261ebfa48e3aa4
Lucas "Disktra" PALMITESSA <contact@disktradev.me>
2019-10-22 00:46:57 +0200

    Replaced raw_input by input

--- Python/rockpaperscissors.py
+++ Python/rockpaperscissors.py
@@ -25,7 +25,7 @@ def game():
 
 def move():
     while True:
-        player = raw_input("\nRock = 1\nPaper = 2\nScissors = 3\nMake a move: ")
+        player = input("\nRock = 1\nPaper = 2\nScissors = 3\nMake a move: ")
         try:
             player = int(player)
             if player in (1, 2, 3):
@@ -55,7 +55,7 @@ def result(player, computer):
             computer_score += 1
 
 def play_again():
-    answer = raw_input("Would you like to play again ? y/n:")
+    answer = input("Would you like to play again ? y/n:")
     if answer in ("y", "Y", "yes", "Yes", "Of course !"):
         return answer
     else:
